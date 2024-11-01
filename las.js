1e55aa9926df51e246284833d399d4e6da0f0fbb
Leosgg11 <leosebagonzalez@gmail.com>
2024-11-01 17:34:57 -0300

    archivos

---
+++ las.js
@@ -0,0 +1,6 @@
+// scripts.js (opcional)
+const form = document.getElementById('contact-form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  // enviar formulario a Formspree o otro servicio
+});
